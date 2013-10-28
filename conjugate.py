0415dfddb18bc8aa10593d80a737cc02c0947cb0
Aaron O'Leary <aaron.oleary@gmail.com>
2013-10-28 13:33:28 +0000

    Basically working solver for given Ui
    Only finds the mode-1, +c solution.

--- conjugate.py
+++ conjugate.py
@@ -1,6 +1,7 @@
 import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.optimize as optimize
 
 # wave amplitudes, speed
 a, b, c = sp.symbols('a, b, c')
@@ -18,41 +19,47 @@ v1, v2, v3 = sp.symbols('v1, v2, v3')
 h1, h2, h3 = sp.symbols('h1, h2, h3')
 
 
-def F(a=a, b=b, v1=v1, v2=v2, v3=v3, h1=h1, h2=h2, h3=h3, s=s):
-    A = (h1 + a) ** 2
-    B = (h2 + b - a) ** 2
-    C = (h3 - b) ** 2
-
-    f1 = - (a + b * s) * v2 ** 2 * h2 ** 2 * A * C
-    f2 = B * (a * v3 ** 2 * h3 ** 2 * A + b * s * v1 ** 2 * h1 ** 2 * C)
-    f3 = A * B * C * (a * (v2 ** 2 - v3 ** 2) + b * s * (v2 ** 2 - v1 ** 2))
-
-    return f1 + f2 + f3
-
-
-def G(a=a, b=b, v1=v1, v2=v2, v3=v3, h1=h1, h2=h2, h3=h3):
-    A = (h1 + a) ** 2
-    B = (h2 + b - a) ** 2
-    C = (h3 - b) ** 2
-
-    fu1 = v1 ** 2 * a ** 3 / A
-    fu2 = v2 ** 2 * (b - a) ** 3 / B
-    fu3 = v3 ** 2 * b ** 3 / C
-
-    return fu1 + fu2 - fu3
-
-
 class FGSolver(object):
     """For given vi, hi, s, find the possible solutions in (a, b)."""
     def __init__(self, s=1, h1=0.2, h2=0.6, h3=0.2, v1=1, v2=1, v3=1):
         # Create functions of (a, b) for these given parameters
-        self.F = F(v1=v1, v2=v2, v3=v3, h1=h1, h2=h2, h3=h3, s=s)
-        self.G = G(v1=v1, v2=v2, v3=v3, h1=h1, h2=h2, h3=h3)
+        self.F = self.F(v1=v1, v2=v2, v3=v3, h1=h1, h2=h2, h3=h3, s=s)
+        self.G = self.G(v1=v1, v2=v2, v3=v3, h1=h1, h2=h2, h3=h3)
         self.f = sp.lambdify((a, b), self.F)
         self.g = sp.lambdify((a, b), self.G)
 
         self.H = h1, h2, h3
         self.V = v1, v2, v3
+
+        self.base = LambBase(s=s, h1=h1, h2=h2, h3=h3)
+
+        # resolution of rough zero search (np.linspace)
+        self.resolution = 1000
+
+    @staticmethod
+    def F(a=a, b=b, v1=v1, v2=v2, v3=v3, h1=h1, h2=h2, h3=h3, s=s):
+        A = (h1 + a) ** 2
+        B = (h2 + b - a) ** 2
+        C = (h3 - b) ** 2
+
+        f1 = - (a + b * s) * v2 ** 2 * h2 ** 2 * A * C
+        f2 = B * (a * v3 ** 2 * h3 ** 2 * A + b * s * v1 ** 2 * h1 ** 2 * C)
+        f3 = A * B * C * (a * (v2 ** 2 - v3 ** 2)
+                          + b * s * (v2 ** 2 - v1 ** 2))
+
+        return f1 + f2 + f3
+
+    @staticmethod
+    def G(a=a, b=b, v1=v1, v2=v2, v3=v3, h1=h1, h2=h2, h3=h3):
+        A = (h1 + a) ** 2
+        B = (h2 + b - a) ** 2
+        C = (h3 - b) ** 2
+
+        fu1 = v1 ** 2 * a ** 3 / A
+        fu2 = v2 ** 2 * (b - a) ** 3 / B
+        fu3 = v3 ** 2 * b ** 3 / C
+
+        return fu1 + fu2 - fu3
 
     def insolutiontriangle(self, ab):
         """Boolean, is ab = (a, b) inside the physical solution
@@ -142,12 +149,13 @@ class FGSolver(object):
             ordered_points = kdt_sort(points)
             ordered_segments[segment] = ordered_points
 
+        # TODO: should this be returned as a list in specific
+        # quadrant order?
         return ordered_segments
 
     def global_rough_zero(self, f):
-        x = np.linspace(-1, 1, 1000)
-        y = np.linspace(-1, 1, 1000)
-        A, B = np.meshgrid(x, y)
+        x = np.linspace(-1, 1, self.resolution)
+        A, B = np.meshgrid(x, x)
         fab = f(A, B)
         # intersection between contour sets is done here:
         # http://stackoverflow.com/questions/17416268/
@@ -158,6 +166,13 @@ class FGSolver(object):
                               for p in line.get_paths())
         a, b = points.T
         return a, b
+        # you could create a kdtree with the F contour points
+        # and one with G, then find nearest neighbours
+        # e.g. kdt = KDTree(f_points)
+        # kdt.query(g_points, k=1, distance_upper_bound=something)
+        # this will pick up curves that very nearly intersect
+        # but not quite - although this is a very particular edge
+        # case.
 
     def zeroG(self, f0):
         """Evaluate G along the lines F=0.
@@ -182,32 +197,124 @@ class FGSolver(object):
         method.
         """
         # nsolve can't take arrays as input for some reason
-        guess = tuple(guess)
+        guess = guess.tolist()
         variables = (a, b)
         equation_set = (self.F, self.G)
-        try:
-            ab = sp.nsolve(equation_set, variables, guess, **kwargs)
-        except:
-            return guess
+        ab = sp.nsolve(equation_set, variables, guess, **kwargs)
         return np.array(ab, dtype=float)
 
     @property
     def roots(self):
         """Calculate the roots of the system."""
         segments = self.rough_zero_ordered()
+        # find where G becomes zero on each branch in each quadrant
+        roughroots = {q: self.zeroG(branch) for q, branch in segments.items()}
 
-        roughzeros = [self.zeroG(segments[s]) for s in segments]
-        # remove duplicates and concatenate
-        guesses = np.row_stack(np.unique(g).reshape((-1, 2))
-                               for g in roughzeros if g.size != 0)
-        enhanced_guesses = [self.enhance(guess) for guess in guesses]
+        # remove duplicates and empty roots
+        # http://stackoverflow.com/questions/8560440
+        def unique(a):
+            order = np.lexsort(a.T)
+            a = a[order]
+            diff = np.diff(a, axis=0)
+            ui = np.ones(len(a), 'bool')
+            ui[1:] = (diff != 0).any(axis=1)
+            return a[ui]
 
-        return enhanced_guesses
+        rough_guesses = {quad: unique(root) if root.size != 0 else None
+                         for quad, root in roughroots.items()}
+
+        # converge on each root with non-linear solver
+        enhanced_guesses = {q: [self.enhance(g) for g in guesses]
+                            if guesses is not None else None
+                            for q, guesses in rough_guesses.items()}
+
+        # remove trivial zero solutions
+        res = 2. / self.resolution
+        nonzero_enhanced_guesses = {}
+        for quadrant, guesses in enhanced_guesses.items():
+            if guesses is None:
+                nonzero_enhanced_guesses[quadrant] = None
+            else:
+                nonzero_enhanced_guesses[quadrant] = [g for g in guesses
+                                                      if np.hypot(*g) > res]
+
+        return nonzero_enhanced_guesses
+
+    def compute_c(self, (a, b)):
+        """Given values of (a, b), calculate
+        the wave speed.
+
+        Uses the relation
+
+        c * Vi = c - Ui
+
+        and
+
+        (c - Ui) ** 2 = f(a, b)
+
+        =>  c ** 2 = f(a, b) / Vi ** 2
+        """
+        fcu1 = self.base.fcu1
+        v1 = self.V[0]
+
+        c2 = fcu1(a, b) / v1 ** 2
+
+        return c2 ** .5
+
+    def compute_U(self, (a, b), c):
+        """For given a, b, c calculate the velocities ui."""
+        u1 = c - self.base.fcu1(a, b) ** .5
+        u2 = c - self.base.fcu2(a, b) ** .5
+        u3 = c - self.base.fcu3(a, b) ** .5
+        return u1, u2, u3
 
 
-# evaluate c+- over a, b
+class GivenUSolver(object):
+    """For given Ui, hi, s determine solutions in (a, b, c)."""
+    def __init__(self, u1=0, u2=0, u3=0, h1=0.2, h2=0.6, h3=0.2, s=1):
+        self.U = u1, u2, u3
+        self.H = h1, h2, h3
+        self.s = s
 
-# this doesn't seem well conditioned?
+        self.x = np.linspace(-1, 1, 1000)
+        self.AB = np.meshgrid(self.x, self.x)
+
+    def solver_given_c(self, c=1):
+        """For a given value of c, obtain solutions."""
+        u1, u2, u3 = self.U
+        h1, h2, h3 = self.H
+        v1 = 1 - u1 / c
+        v2 = 1 - u2 / c
+        v3 = 1 - u3 / c
+
+        solver = FGSolver(s=self.s, v1=v1, v2=v2, v3=v3, h1=h1, h2=h2, h3=h3)
+        return solver
+
+    def find_c(self, c, quadrant):
+        solver = self.solver_given_c(c=c)
+        r = solver.roots[quadrant]
+        try:
+            return c - solver.compute_c(r[0])
+        except ValueError:
+            # catch the case that there is no root for this c
+            # then mask this up by giving negative return.
+            # return negative because this usually happens for small
+            # guess c, when we expect c - solver.compute_c to be
+            # negative
+            print c, r
+            return -1
+
+    def root_find(self):
+        # FIXME: select root that will have largest c
+        # FIXME: Shouldn't we find c for all roots?
+        # mode-1 solution
+        root = 'upper_right'
+        # TODO: use some try, except logic to catch non existent roots
+        c = optimize.brentq(self.find_c, 0.2, 1, args=(root,))
+        solver = self.solver_given_c(c=c)
+        r = solver.roots[root][0]
+        return {'a, b': r, 'c': c}
+
 
 # alternative: formulate lambs base equations as 3 equations in
 # (a, b, c).
@@ -218,15 +325,32 @@ class FGSolver(object):
 
 # are these going to pick up a=b=c=0 as trivial solution??
 # how many roots are there going to be? at least two, probably 4.
-
-# The equations in Lamb's notation:
-
 class LambBase(object):
+    """Lamb 2000, base equation set."""
+    def __init__(self, s=s, h1=h1, h2=h2, h3=h3):
+        """An instance initialised with numerical values will
+        allow calculation of c - ui as a function of (a, b).
+        """
+        # Create some useful relations
+        A = (h1 + a)
+        B = (h2 + b - a)
+        C = (h3 - b)
+
+        self.Ai = A, B, C
+
+        H1 = A ** 2 - h1 ** 2
+        H2 = B ** 2 - h2 ** 2
+        H3 = C ** 2 - h3 ** 2
+
+        self.Hi = H1, H2, H3
+
+        self.alpha = 2 * a / s
+        self.beta = 2 * b
+
     @staticmethod
     def f1(a=a, b=b, c=c, s=s, u1=u1, u2=u2, u3=u3, h1=h1, h2=h2, h3=h3):
         A = (h1 + a)
         B = (h2 + b - a)
-        C = (h3 - b)
 
         fu1 = (c - u1) ** 2 * (1 - (h1 / A) ** 2)
         fu2 = (c - u2) ** 2 * (1 - (h2 / B) ** 2)
@@ -235,7 +359,6 @@ class LambBase(object):
 
     @staticmethod
     def f2(a=a, b=b, c=c, s=s, u1=u1, u2=u2, u3=u3, h1=h1, h2=h2, h3=h3):
-        A = (h1 + a)
         B = (h2 + b - a)
         C = (h3 - b)
 
@@ -256,32 +379,49 @@ class LambBase(object):
 
         return fu1 + fu2 - fu3
 
-    A = (h1 + a)
-    B = (h2 + b - a)
-    C = (h3 - b)
-
-    H1 = A - h1 ** 2
-    H2 = A - h2 ** 2
-    H3 = A - h3 ** 2
-
-    alpha = 2 * a / s
-    beta = 2 * b
-
+    @property
     def cu1(self):
         """(c - U1) ** 2 as a function of (a, b)."""
+        H1, H2, H3 = self.Hi
+        A, B, C = self.Ai
+        alpha, beta = self.alpha, self.beta
+
         n = H3 * alpha * (b - a) ** 3 - H2 * (alpha + beta) * b ** 3
         d = H2 * H3 * a ** 3 + H1 * H3 * (b - a) ** 3 - H1 * H2 * b ** 3
         return A ** 2 * n / d
 
-    def cu2():
+    @property
+    def cu2(self):
         """(c - U2) ** 2 as a function of (a, b), given (c - U1) ** 2."""
-        return (cu1() / A ** 2 * H1 - alpha) / H2
+        H1, H2, H3 = self.Hi
+        A, B, C = self.Ai
+        alpha, beta = self.alpha, self.beta
 
-    def cu3():
+        return B ** 2 * (H1 * (self.cu1() / A ** 2) - alpha) / H2
+
+    @property
+    def cu3(self):
         """(c - U3) ** 2 as a function of (a, b), given (c - U1) ** 2."""
-        return (cu1() / A ** 2 * H1 - (alpha + beta)) / H3
+        H1, H2, H3 = self.Hi
+        A, B, C = self.Ai
+        alpha, beta = self.alpha, self.beta
 
+        return C ** 2 * (H1 * (self.cu1() / A ** 2) - (alpha + beta)) / H3
 
+    @property
+    def fcu1(self):
+        """Create a function f(a, b) = (c - U1) ** 2."""
+        return sp.lambdify((a, b), self.cu1)
+
+    @property
+    def fcu2(self):
+        """Create a function f(a, b) = (c - U2) ** 2."""
+        return sp.lambdify((a, b), self.cu2)
+
+    @property
+    def fcu3(self):
+        """Create a function f(a, b) = (c - U3) ** 2."""
+        return sp.lambdify((a, b), self.cu3)
 
 
 class Example_nsolve(object):
@@ -332,21 +472,6 @@ class Example_nsolve(object):
         guesses = self.array_of_guesses()
         return guesses
 
-
-class SolveGivenU(object):
-    pass
-
-
-
-
-
-
-# TODO: create an array of initial guesses in the space (a, b, c)
-# bounded by the solution triangle.
-
-# TODO: catch non convergence of solver
-# TODO: condense array of solutions to unique solutions.
-# TODO: divide these into mode-1, mode-2
 
 # TODO: for a specific (S, d0) compute the supercritical solution
 # curve, defining (h, U0, d1c) along its length.
